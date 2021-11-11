d088443888c9b4682b586f3008846a6b650acfa5
vinicius martins <vinicius.cross07@gmail.com>
2021-11-11 16:50:52 -0300

    jest

--- __tests__/App.test.js
+++
@@ -1,5 +0,0 @@
-import "react-native";
-import React from "react";
-import App from "../App";
-
-// Note: escreva seu test aqui
---
+++ src/__tests__/src/screens/ExamplePage.test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+import { ExamplePage } from "../../../screens/ExamplePage";
+
+test("renders correctly ExamplePage", () => {
+  render(<ExamplePage />);
+});
