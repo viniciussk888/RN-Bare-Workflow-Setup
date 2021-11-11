dd00cd735f32baf9b62720e4fe72faf2e26d463f
vinicius martins <vinicius.cross07@gmail.com>
2021-11-11 16:53:33 -0300

    test

--- src/__tests__/src/screens/ExamplePage/index.test.tsx
+++ src/__tests__/src/screens/ExamplePage/index.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react-native";
-import { ExamplePage } from "../../../screens/ExamplePage";
+import { ExamplePage } from "../../../../screens/ExamplePage";
 
 test("renders correctly ExamplePage", () => {
   render(<ExamplePage />);
